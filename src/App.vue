59bcb00bb85ffa50e22541fa71d6baee2bd52bd3
mickey000777 <45608259+mickey000777@users.noreply.github.com>
2019-01-03 13:21:03 +0100

    eerste upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="valuta">
+        <valuta :waarden="waarden"/>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Valuta from './components/Valuta.vue'
 
 export default {
   name: 'app',
+      data(){
+        return {
+          waarden: [
+
+            {
+              cijfer: 1.124, 
+              munt: "Britse pond",
+            },
+
+            {
+              cijfer: 0.8803, 
+              munt: "US dollar",
+            },
+
+            {
+              cijfer: 0.0983, 
+              munt: "Zweedse kroon",
+            },
+
+            {
+              cijfer: 1.2405, 
+              munt: "Jordaanse dinar",
+            },            
+
+            {
+              cijfer: 0.008, 
+              munt: "Vanuatu vatu",
+            },
+
+            {
+              cijfer: 2.2872, 
+              munt: "Omaanse rial",
+            },
+          ],
+
+        }
+
+    },
   components: {
-    HelloWorld
+    Valuta
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.valuta {
+  display: grid;
+  justify-content: center;
+  width: 18em;
+  background-color: gray;
+  padding: 0 0 .5em 0;
+}
+.inputValuta {
+  margin: .5em 0 1em 0;
+  width: 15em;
+}
+  input[type=number]::-webkit-inner-spin-button,
+  input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+input[type=number] {
+    -moz-appearance:textfield;
 }
 </style>
---
+++ src/components/Valuta.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <label>€ </label>
+    <input class="inputValuta" type="number" placeholder="geef een waarde in in €" v-model="getal">
+
+    <ul v-if="getal">
+        <li v-for="(waarde, index) of waarden" 
+            :key="index" 
+            :class=" waarde.cijfer < 1 ? 'groen' : 'rood' ">
+            {{(index + 1)}}) {{(getal / waarde.cijfer).toFixed(2)}} - {{waarde.munt}}</li>
+        
+    </ul>
+</div>
+
+
+</template>
+
+<script>
+export default {
+
+    data(){
+        return {
+            getal: "",
+        }
+    },
+
+    props: {
+        waarden: {
+            type: Array,
+        },
+    },
+    methods: {
+
+        }
+    }
+
+</script>
+
+<style>
+label {
+    color: white;
+}
+ul {
+    margin: 0;
+    padding: 0;
+}
+li {
+    list-style-type: none;
+    padding: .2em;
+    margin: 0;
+}
+.rood {
+    color: red;
+}
+.groen {
+    color:palegreen;
+}
+</style>
